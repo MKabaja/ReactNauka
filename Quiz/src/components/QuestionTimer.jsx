5dc35fed6d3507edf0d5555d36b6f4a29db2d087
Wichern94 <Mateusz.kabaja@gmail.com>
2026-02-26 11:53:49 +0100

    quiz app finished

--- Quiz/src/components/QuestionTimer.jsx
+++ Quiz/src/components/QuestionTimer.jsx
@@ -1,27 +1,33 @@
 import { useState, useEffect } from 'react';
-export default function QuestionTimer({ timeout, onTimeout }) {
+export default function QuestionTimer({ timeout, onTimeout, mode }) {
 	const [remainingTime, setRemainingTime] = useState(timeout);
 	const devideValue = timeout / 200;
 
 	useEffect(() => {
 		console.log('setting timeout');
-		setTimeout(onTimeout, timeout);
+		const timer = setTimeout(onTimeout, timeout);
+		return () => clearTimeout(timer);
 	}, [timeout, onTimeout]);
 
 	useEffect(() => {
 		console.log('setting interval');
-		setInterval(() => {
+		const progress = setInterval(() => {
 			setRemainingTime(
 				(prevRemainingTime) => prevRemainingTime - devideValue,
 			);
 		}, devideValue);
-	}, []);
+
+		return () => {
+			clearInterval(progress);
+		};
+	}, [timeout]);
 
 	return (
 		<progress
 			id='question-time'
 			max={timeout}
 			value={remainingTime}
+			className={mode}
 		></progress>
 	);
 }
--- Quiz/src/components/Quiz.jsx
+++ Quiz/src/components/Quiz.jsx
@@ -1,10 +1,7 @@
 import { useState, useCallback } from 'react';
 import quizData from '../questions.js';
-import quizCompleteImg from '../assets/quiz-complete.png';
-import Progressbar from './ProgressBar.jsx';
-import QuestionTimer from './QuestionTimer.jsx';
-
-const TIMER = 15000;
+import Summary from './Summary.jsx';
+import Question from './Question.jsx';
 
 export default function Quiz() {
 	const [userAnswers, setUserAnswers] = useState([]);
@@ -19,54 +16,24 @@ export default function Quiz() {
 			return [...prevUserAnswers, selectedAnswer];
 		});
 	}, []);
+
 	const handleSkipAnswer = useCallback(
 		() => handleSelectAnswer(null),
 
 		[handleSelectAnswer],
 	);
 	if (quizIsComplete) {
-		return (
-			<div id='summary'>
-				<h2>Quiz Complete!</h2>
-				<img
-					src={quizCompleteImg}
-					alt='Quiz complete image'
-				/>
-			</div>
-		);
+		return <Summary userAnswers={userAnswers} />;
 	}
-	const shuffledAnswers = [...quizData[activeQuestionIndex].answers];
-	shuffledAnswers.sort(() => Math.random() - 0.5);
 
 	return (
 		<section id='quiz'>
-			<div id='question'>
-				<QuestionTimer
-					timeout={TIMER}
-					onTimeout={handleSkipAnswer}
-				/>
-				<h2>{quizData[activeQuestionIndex].text}</h2>
-				<ul id='answers'>
-					{shuffledAnswers.map((answer, index) => {
-						return (
-							<li
-								key={index}
-								className='answer'
-							>
-								<button
-									onClick={() => handleSelectAnswer(answer)}
-								>
-									{answer}
-								</button>
-							</li>
-						);
-					})}
-				</ul>
-				{/* <Progressbar
-					timer={TIMER}
-					onFinish={handleFinish}
-				/> */}
-			</div>
+			<Question
+				key={activeQuestionIndex}
+				index={activeQuestionIndex}
+				onSelectAnswer={handleSelectAnswer}
+				onSkipAnswer={handleSkipAnswer}
+			/>
 		</section>
 	);
 }
