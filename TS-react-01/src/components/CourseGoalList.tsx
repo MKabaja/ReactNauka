63ac6dacd75f5d4f0590fdac8d2ac95b94778b0f
Wichern94 <Mateusz.kabaja@gmail.com>
2026-02-27 23:03:03 +0100

    Union Type props

--- TS-react-01/src/components/CourseGoalList.tsx
+++ TS-react-01/src/components/CourseGoalList.tsx
@@ -1,10 +1,11 @@
-import { ReactNode } from 'react';
+import { type ReactNode } from 'react';
 import { type CourseGoalType } from '../App.tsx';
 import CourseGoal from './CourseGoal.tsx';
+import InfoBox from './InfoBox.tsx';
 
 interface CourseGoalListProps {
 	goals: CourseGoalType[];
-	children?: ReactNode;
+
 	onDeleteGoal: (id: number) => void;
 }
 
@@ -12,19 +13,42 @@ export default function CourseGoalList({
 	goals,
 	onDeleteGoal,
 }: CourseGoalListProps) {
+	if (goals.length === 0) {
+		return (
+			<InfoBox mode='hint'>
+				Nie masz jeszcze żadnych celów. Dodaj jakieś!
+			</InfoBox>
+		);
+	}
+	let warningBox: ReactNode;
+
+	if (goals.length >= 4) {
+		warningBox = (
+			<InfoBox
+				mode='warning'
+				severity='medium'
+			>
+				Nazbierałeś za dużo celów!
+			</InfoBox>
+		);
+	}
+
 	return (
-		<ul>
-			{goals.map((goal) => (
-				<li key={goal.id}>
-					<CourseGoal
-						title={goal.title}
-						onDelete={onDeleteGoal}
-						id={goal.id}
-					>
-						<p>{goal.description}</p>
-					</CourseGoal>
-				</li>
-			))}
-		</ul>
+		<>
+			{warningBox}
+			<ul>
+				{goals.map((goal) => (
+					<li key={goal.id}>
+						<CourseGoal
+							title={goal.title}
+							onDelete={onDeleteGoal}
+							id={goal.id}
+						>
+							<p>{goal.description}</p>
+						</CourseGoal>
+					</li>
+				))}
+			</ul>
+		</>
 	);
 }
