af61cd2299751f97e8bc41020190de53e9760abd
Wichern94 <Mateusz.kabaja@gmail.com>
2026-03-08 22:03:13 +0100

    Initialize React project with TypeScript
    - add Types to AvailablePlaces.tsx, Places.tsx,
    - add fetching logic

--- REACT-http-req/src/components/AvailablePlaces.tsx
+++ REACT-http-req/src/components/AvailablePlaces.tsx
@@ -2,18 +2,50 @@ import Places from './Places.jsx';
 import { useEffect, useState } from 'react';
 
 type AvailablePlacesProps = {
-	onSelectPlace: (placeId: string) => void;
+	onSelectPlace: (place: Place) => void;
 };
-const places = localStorage.getItem('places');
+export type Place = {
+	id: string;
+	title: string;
+	image: {
+		src: string;
+		alt: string;
+	};
+	lat: number;
+	lon: number;
+};
+type AvailablePlacesState = Place[] | null[];
+type AvailablePlacesResponse = {
+	places: Place[];
+};
 
 export default function AvailablePlaces({
 	onSelectPlace,
 }: AvailablePlacesProps) {
-	const [availablePlaces, setAvailablePlaces] = useState(places);
+	const [isFetching, setIsFetching] = useState<boolean>(false);
+	const [availablePlaces, setAvailablePlaces] =
+		useState<AvailablePlacesState>([]);
+
+	useEffect(() => {
+		async function fetchPlaces(url: string) {
+			setIsFetching(true);
+			const response = await fetch(url);
+			if (!response.ok) {
+				throw new Error(`HTTP error! status: ${response.status}`);
+			}
+			const responseData: AvailablePlacesResponse = await response.json();
+			setAvailablePlaces(responseData.places);
+			setIsFetching(false);
+		}
+		fetchPlaces('http://localhost:3000/places');
+	}, []);
+
 	return (
 		<Places
 			title='Available Places'
-			places={[]}
+			places={availablePlaces}
+			loadingText='Loading places...'
+			isLoading={isFetching}
 			fallbackText='No places available.'
 			onSelectPlace={onSelectPlace}
 		/>
---
+++ REACT-http-req/src/components/Places.tsx
@@ -0,0 +1,50 @@
+import { type Place } from './AvailablePlaces';
+
+type PlacesProps = {
+	title: string;
+	places: Place[] | null[];
+	fallbackText: string;
+	onSelectPlace: (place: Place) => void;
+	isLoading: boolean;
+	loadingText: string;
+};
+
+export default function Places({
+	title,
+	places,
+	fallbackText,
+	onSelectPlace,
+	isLoading,
+	loadingText,
+}: PlacesProps) {
+	console.log(places);
+	return (
+		<section className='places-category'>
+			<h2>{title}</h2>
+			{isLoading && <p className='fallback-text'>{loadingText}</p>}
+			{!isLoading && places.length === 0 && (
+				<p className='fallback-text'>{fallbackText}</p>
+			)}
+			{!isLoading && places.length > 0 && (
+				<ul className='places'>
+					{places.map((place) => (
+						<li
+							key={place?.id}
+							className='place-item'
+						>
+							{place && (
+								<button onClick={() => onSelectPlace(place)}>
+									<img
+										src={`http://localhost:3000/${place.image.src}`}
+										alt={place.image.alt}
+									/>
+								</button>
+							)}
+							<h3>{place?.title}</h3>
+						</li>
+					))}
+				</ul>
+			)}
+		</section>
+	);
+}
